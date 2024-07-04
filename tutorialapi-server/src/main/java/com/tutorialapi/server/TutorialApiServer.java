88dbd2af3ffea78048ca9f2fc03d5c3a236b2a8c
tejumolamann <tejumolamann@gmail.com>
2024-07-04 19:30:47 +0100

    jetty server updates, dependency updates

--- tutorialapi-server/src/main/java/com/tutorialapi/server/TutorialApiServer.java
+++ tutorialapi-server/src/main/java/com/tutorialapi/server/TutorialApiServer.java
@@ -1,11 +1,18 @@
 package com.tutorialapi.server;
 
+import org.eclipse.jetty.ee10.servlet.DefaultServlet;
+import org.eclipse.jetty.ee10.servlet.ServletContextHandler;
+import org.eclipse.jetty.ee10.servlet.ServletHolder;
 import org.eclipse.jetty.http.HttpScheme;
 import org.eclipse.jetty.http.HttpVersion;
 import org.eclipse.jetty.server.*;
+import org.eclipse.jetty.util.resource.URLResourceFactory;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
+import org.glassfish.jersey.servlet.ServletContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
 
 public class TutorialApiServer {
     private static final Logger LOGGER = LoggerFactory.getLogger(TutorialApiServer.class);
@@ -17,6 +24,7 @@ public class TutorialApiServer {
         httpsConfiguration.setSecurePort(8443);
         httpsConfiguration.addCustomizer(new SecureRequestCustomizer());
         httpsConfiguration.setSendServerVersion(false);
+        httpsConfiguration.setSendDateHeader(false);
 
         HttpConnectionFactory httpConnectionFactory = new HttpConnectionFactory(httpsConfiguration);
 
@@ -37,6 +45,21 @@ public class TutorialApiServer {
 
         server.addConnector(httpsConnector);
 
+        ServletContextHandler servletContextHandler = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
+        servletContextHandler.setContextPath("/");
+        servletContextHandler.setBaseResource(
+                new URLResourceFactory().newResource(
+                        new File("tutorialapi-server/src/main/resources/www").toURI().toURL()
+                )
+        );
+        servletContextHandler.addServlet(DefaultServlet.class, "/");
+
+        server.setHandler(servletContextHandler);
+
+        ServletHolder apiServletHolder = servletContextHandler.addServlet(ServletContainer.class, "/api/*");
+        apiServletHolder.setInitParameter("jakarta.ws.rs.Application", "TODO");
+
+        LOGGER.info("Server starting");
         server.start();
         server.join();
     }
