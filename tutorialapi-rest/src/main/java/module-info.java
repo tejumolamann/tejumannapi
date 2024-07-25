449940cf8838fa2ab23f0e6861c7f0e5dea0c4e0
tejumolamann <tejumolamann@gmail.com>
2024-07-25 16:01:19 +0100

    Fixed module issue
    Fixed modules-info error and suppressed maven compiler warnings relating to requires directive for an automatic module.

--- tutorialapi-rest/src/main/java/module-info.java
+++ tutorialapi-rest/src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module tutorialapi.rest {
+    exports com.tutorialapi.rest;
     requires jakarta.ws.rs;
     requires jersey.server;
+    requires jersey.common;
     requires org.glassfish.hk2.api;
     requires org.slf4j;
+
+    opens com.tutorialapi.rest to jersey.server;
 }
